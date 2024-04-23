da6b12044402792499abd33f9c5273046cb03079
shingugitvr000 <shingugitvr000@gmail.com>
2024-04-23 18:33:20 +0900

    [Feat] 스타트 추가

--- Assets/Script/MovePlayer.cs
+++ Assets/Script/MovePlayer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using TMPro;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
@@ -12,6 +13,9 @@ public class MovePlayer : MonoBehaviour
     public float moveSpeed = 5f;
     public float moveSpeed1 = 5f;
 
+    public float StartTime = 3;
+    public TMP_Text startFont;
+
     public Vector3 minPosition = new Vector3(-15f, -10f, -15f);
     public Vector3 maxPosition = new Vector3(15f, 100f, 15f);
     public Vector3 originalPosition = new Vector3(0f,0f,0f);
@@ -19,63 +23,54 @@ public class MovePlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        _Rigidbody.velocity = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        StartTime -= Time.deltaTime;
+        startFont.text = StartTime.ToString("F0");
+
+        if (StartTime <= 0)
         {
-            _Rigidbody.velocity = Vector3.zero;
-            _Rigidbody.AddForce(Vector3.up * Force * Time.deltaTime, ForceMode.Impulse);
-            _Rigidbody.AddForce(Vector3.left * Force1 * Time.deltaTime, ForceMode.Impulse);
+            StartTime = 0;
+            startFont.text = "";
 
-            //this.gameObject.transform.position += new Vector3(Force, Force, 0.0f);
+            if (Input.GetMouseButtonDown(0))
+            {
+                _Rigidbody.velocity = Vector3.zero;
+                _Rigidbody.AddForce(Vector3.up * Force, ForceMode.VelocityChange);
+                _Rigidbody.AddForce(Vector3.left * Force1, ForceMode.Impulse);
+            }
 
-            //Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //targetPosition.z = 0;
-            //targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
-            //targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
-            //targetPosition.z = Mathf.Clamp(targetPosition.z, minPosition.z, maxPosition.z);
+            if (Input.GetMouseButtonDown(1))
+            {
+                _Rigidbody.velocity = Vector3.zero;
+                _Rigidbody.AddForce(Vector3.up * Force, ForceMode.VelocityChange);
+                _Rigidbody.AddForce(Vector3.right * Force1, ForceMode.Impulse);
+            }
 
-            //transform.position = targetPosition;
+            Vector3 clasmpedPosition = transform.position;
+            clasmpedPosition.x = Mathf.Clamp(clasmpedPosition.x, minPosition.x, maxPosition.x);
+            clasmpedPosition.y = Mathf.Clamp(clasmpedPosition.y, minPosition.y, maxPosition.y);
+            clasmpedPosition.z = Mathf.Clamp(clasmpedPosition.z, minPosition.z, maxPosition.z);
+
+            if (transform.position != clasmpedPosition)
+            {
+                transform.position = originalPosition;
+            }
         }
 
-        if (Input.GetMouseButtonDown(1))
-        {
-            _Rigidbody.velocity = Vector3.zero;
-            _Rigidbody.AddForce(Vector3.up * Force * Time.deltaTime, ForceMode.Impulse);
-            _Rigidbody.AddForce(Vector3.right * Force1 * Time.deltaTime, ForceMode.Impulse);
-
-            //this.gameObject.transform.position += new Vector3(-Force, Force, 0.0f);
-
-            //Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            //targetPosition.z = 0;
-            //targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
-            //targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
-            //targetPosition.z = Mathf.Clamp(targetPosition.z, minPosition.z, maxPosition.z);
-
-            //transform.position = targetPosition;
-
-        }
-
-        Vector3 clasmpedPosition = transform.position;
-        clasmpedPosition.x = Mathf.Clamp(clasmpedPosition.x, minPosition.x, maxPosition.x);
-        clasmpedPosition.y = Mathf.Clamp(clasmpedPosition.y, minPosition.y, maxPosition.y);
-        clasmpedPosition.z = Mathf.Clamp(clasmpedPosition.z, minPosition.z, maxPosition.z);
-
-        if (transform.position != clasmpedPosition)
-        {
-            transform.position = originalPosition;
-        }
 
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision != null)
         {
-            gameObject.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+            _Rigidbody.velocity = Vector3.zero;
+            gameObject.transform.position = new Vector3(0.0f, 1.0f, 0.0f);
+            SceneManager.LoadScene("GameScene");
             Debug.Log(collision.gameObject.name);
         }
     }
