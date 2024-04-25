554c24ec97aa6f986bd476551d8b56bdd1968b4c
ssoyeon <ssuu2039@gmail.com>
2024-04-25 16:35:06 +0900

    [feat] 닿아도 마우스 입력X, 나무 위치

--- Assets/Script/MovePlayer.cs
+++ Assets/Script/MovePlayer.cs
@@ -5,9 +5,12 @@ using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using DG.Tweening;
+using UnityEditor;
 
 public class MovePlayer : MonoBehaviour
 {
+    private bool canMove = true;
+
     public GameObject rander;
     public Rigidbody _Rigidbody;
     public int Force = 100;
@@ -42,20 +45,23 @@ public class MovePlayer : MonoBehaviour
             startFont.text = "";
             _Rigidbody.useGravity = true;
 
-            //마우스 좌클릭 시에 왼쪽 위로 올라감
-            if (Input.GetMouseButtonDown(0))
+            if (canMove == true)
             {
-                _Rigidbody.velocity = Vector3.zero;
-                _Rigidbody.AddForce(Vector3.up * Force, ForceMode.VelocityChange);
-                _Rigidbody.AddForce(Vector3.left * Force1, ForceMode.Impulse);
-            }
+                //마우스 좌클릭 시에 왼쪽 위로 올라감
+                if (Input.GetMouseButtonDown(0))
+                {
+                    _Rigidbody.velocity = Vector3.zero;
+                    _Rigidbody.AddForce(Vector3.up * Force, ForceMode.VelocityChange);
+                    _Rigidbody.AddForce(Vector3.left * Force1, ForceMode.Impulse);
+                }
 
-            //마우스 우클릭 시에 오른쪽 위로 올라감
-            else if (Input.GetMouseButtonDown(1))
-            {
-                _Rigidbody.velocity = Vector3.zero;
-                _Rigidbody.AddForce(Vector3.up * Force, ForceMode.VelocityChange);
-                _Rigidbody.AddForce(Vector3.right * Force1, ForceMode.Impulse);
+                //마우스 우클릭 시에 오른쪽 위로 올라감
+                else if (Input.GetMouseButtonDown(1))
+                {
+                    _Rigidbody.velocity = Vector3.zero;
+                    _Rigidbody.AddForce(Vector3.up * Force, ForceMode.VelocityChange);
+                    _Rigidbody.AddForce(Vector3.right * Force1, ForceMode.Impulse);
+                }
             }
 
             //x,y,z 써놓은 좌표 이상 나가면 재시작
@@ -73,11 +79,17 @@ public class MovePlayer : MonoBehaviour
 
 
     }
+
     private void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.CompareTag("Boundary"))
+        {
+            canMove = false;
+        }
+
         if (collision != null)
         {
-            _Rigidbody.velocity = Vector3.zero;
+                _Rigidbody.velocity = Vector3.zero;
             rander.transform.DOPunchScale(Vector3.one, 0.7f).OnComplete(() =>
             {
                 
