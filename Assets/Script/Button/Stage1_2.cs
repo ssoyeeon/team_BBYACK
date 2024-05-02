a6e0476e80a5770ae56dd9d33f7da3788b0a237b
ssoyeeon <ssuu2039@gmail.com>
2024-05-02 20:01:48 +0900

    [feat] 스테이지 추가 및 게임오버씬 바꿈

---
+++ Assets/Script/Button/Stage1_2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage1_2 : MonoBehaviour
+{
+    public void StageMain()
+    {
+        SceneManager.LoadScene("Stage1-2");
+    }
+}
