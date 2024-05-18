25f4af738332991fb06c4dc8c9de7dd486efe39d
ssoyeeon <ssuu2039@gmail.com>
2024-05-18 16:11:00 +0900

    [feat] 4-7 보스 스테이지 캐릭터 변경된거 넣고 모드 입히깅ㅇ

---
+++ Assets/Script/Dotween/Ball.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public float duration;
+
+    public SpriteRenderer spr;
+
+    void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        spr.DOFade(0f, duration).SetLoops(-1, LoopType.Yoyo);           //DOFade∏¶ ≈Î«ÿ ∞¯¿ª ±Ù∫˝¿”                            
+    }
+}
