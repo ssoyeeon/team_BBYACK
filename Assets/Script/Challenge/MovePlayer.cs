0e3df1ad020964fa9bbc1fadac092c9ae164c39a
ssoyeeon <ssuu2039@gmail.com>
2024-06-10 18:45:40 +0900

    [feat] 비ㅏㅇ어막

--- Assets/Script/Challenge/MovePlayer.cs
+++ Assets/Script/Challenge/MovePlayer.cs
@@ -100,8 +100,6 @@ public class MovePlayer : MonoBehaviour
                     _Rigidbody.velocity = Vector3.zero;
                     _Rigidbody.AddForce(Vector3.up * Force, ForceMode.VelocityChange);
                     _Rigidbody.AddForce(Vector3.left * Force1, ForceMode.Impulse);
-
-                    SoundManager.instance.PlaySound("Cannon");
                 }
 
                 //마우스 우클릭 시에 오른쪽 위로 올라감
@@ -110,8 +108,6 @@ public class MovePlayer : MonoBehaviour
                     _Rigidbody.velocity = Vector3.zero;
                     _Rigidbody.AddForce(Vector3.up * Force, ForceMode.VelocityChange);
                     _Rigidbody.AddForce(Vector3.right * Force1, ForceMode.Impulse);
-
-                    SoundManager.instance.PlaySound("Cannon");
                 }
             }
 
@@ -123,8 +119,7 @@ public class MovePlayer : MonoBehaviour
 
             if (transform.position != clasmpedPosition)
             {
-                gameObject.transform.position = new Vector3(0.0f, 1.0f, 0.0f);
-                SceneManager.LoadScene("C.GameOverScene");             
+                gameObject.transform.position = new Vector3(0.0f, 1.0f, 0.0f);      
             }
         }
     }
