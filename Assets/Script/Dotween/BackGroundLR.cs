0f032362e9f6a3e5e8cbeef00c9fbb4b08742872
gy1022 <bokowife2@naver.com>
2024-07-23 14:56:01 +0900

    [Feat] enum 사용

--- Assets/Script/Dotween/BackGroundLR.cs
+++ Assets/Script/Dotween/BackGroundLR.cs
@@ -7,28 +7,34 @@ using DG.Tweening;
 public class BackGroundLR : MonoBehaviour
 {
     public int num;
+    public enum State
+    {
+        Default,
+        Star,
+        Up,
+        Right,
+        Left
+    }
+    public State state = State.Default;
     public int L = 10;
     public int U = 1;
     public Vector3 R;
 
     void Start()
     {
-        if (num == 1)
+        if (state == State.Left)
         {
             this.transform.DOMoveX(L, 30).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
         }
-
-        if (num == 2)
+        else if(state == State.Up)
         {
             this.transform.DOMoveY(U,17).SetEase(Ease.Linear);
         }
-
-        if(num == 3)
+        else if (state == State.Right)
         {
             this.transform.DOMoveX(L, 30).SetEase(Ease.Linear);
         }
-
-        if(num == 4)
+        else if(state == State.Star)
         {
             this.transform.DORotate(R, 30).SetLoops(-1, LoopType.Yoyo);
         }
