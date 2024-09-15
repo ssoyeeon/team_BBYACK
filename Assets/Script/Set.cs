8013d240f23bc51a100a24119cd153b1e792b746
ssoyeeon <ssuu2039@gmail.com>
2024-09-15 17:52:34 +0900

    [fix] 빌드 및 씬

--- Assets/Script/Set.cs
+++ Assets/Script/Set.cs
@@ -6,19 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class Set : MonoBehaviour
 {
-    //public enum Setting
-    //{
-    //    GameOut,
-    //    ReStart,
-    //   Home
-    //}
-    //public void Update()
-    //{
-    //    if(Input.GetMouseButtonDown(0))
-    //    Application.Quit();
-
-    //    Scene scene = SceneManager.GetActiveScene();
-    //    Debug.Log(scene.name);
-    //    SceneManager.LoadScene(scene.name); 
-    //}
+    public void Quit()
+    {
+            Application.Quit();
+    }
 }
